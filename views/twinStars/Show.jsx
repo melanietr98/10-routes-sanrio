2f8c54853a1b03cb3278e4e6a76a64e778a3436e
Melanie R <102996216+melanietr98@users.noreply.github.com>
2023-03-14 00:43:55 -0400

    Update Show.jsx

--- views/twinStars/Show.jsx
+++ views/twinStars/Show.jsx
@@ -4,7 +4,7 @@ function Show(props) {
     return (
         <div>
             <h1>Show View</h1>
-            <p>Their <strong>{props.twinStars.name}</strong> are {props.twinStars.size}</p>
+            <p> <strong>{props.twinStars.name}</strong> is {props.twinStars.size}</p>
             <p>their color is <span style={{ color: props.twinStars.color }}>{props.twinStars.color}</span></p>
 
             <br />
