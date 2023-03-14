a8f38e7f7aac190e1ea1d60e16867239d1f3aa68
Melanie R <102996216+melanietr98@users.noreply.github.com>
2023-03-14 00:41:20 -0400

    Update Show.jsx

--- views/pochacco/Show.jsx
+++ views/pochacco/Show.jsx
@@ -4,7 +4,7 @@ function Show(props) {
     return (
         <div>
             <h1>Show View</h1>
-            <p>The <strong>{props.pochacco.name}</strong> is {props.pochacco.size}</p>
+            <p> <strong>{props.pochacco.name}</strong> is {props.pochacco.size}</p>
             <p>Its color is <span style={{ color: props.pochacco.color }}>{props.pochacco.color}</span></p>
 
             <br />
