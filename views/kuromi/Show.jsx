733784e62ec9609fa9beb7e7398914889931ae6b
Melanie R <102996216+melanietr98@users.noreply.github.com>
2023-03-14 00:40:59 -0400

    Update Show.jsx

--- views/kuromi/Show.jsx
+++ views/kuromi/Show.jsx
@@ -4,7 +4,7 @@ function Show(props) {
     return (
         <div>
             <h1>Show View</h1>
-            <p>The <strong>{props.kuromi.name}</strong> is {props.kuromi.size}</p>
+            <p> <strong>{props.kuromi.name}</strong> is {props.kuromi.size}</p>
             <p>Its color is <span style={{ color: props.kuromi.color }}>{props.kuromi.color}</span></p>
 
             <br />
