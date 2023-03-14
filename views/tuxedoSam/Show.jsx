808f5bdc760d1e61ec4ba2ae992fe67602246f49
Melanie R <102996216+melanietr98@users.noreply.github.com>
2023-03-14 00:42:08 -0400

    Update Show.jsx

--- views/tuxedoSam/Show.jsx
+++ views/tuxedoSam/Show.jsx
@@ -4,7 +4,7 @@ function Show(props) {
     return (
         <div>
             <h1>Show View</h1>
-            <p>The <strong>{props.tuxedoSam.name}</strong> is {props.tuxedoSam.size}</p>
+            <p> <strong>{props.tuxedoSam.name}</strong> is {props.tuxedoSam.size}</p>
             <p>Its color is <span style={{ color: props.tuxedoSam.color }}>{props.tuxedoSam.color}</span></p>
 
             <br />
