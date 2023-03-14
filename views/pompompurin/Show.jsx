309a04dd6d81cba4e4ddb966f44ad7d44bb790eb
Melanie R <102996216+melanietr98@users.noreply.github.com>
2023-03-14 00:41:35 -0400

    Update Show.jsx

--- views/pompompurin/Show.jsx
+++ views/pompompurin/Show.jsx
@@ -4,7 +4,7 @@ function Show(props) {
     return (
         <div>
             <h1>Show View</h1>
-            <p>The <strong>{props.pompompurin.name}</strong> is {props.pompompurin.size}</p>
+            <p> <strong>{props.pompompurin.name}</strong> is {props.pompompurin.size}</p>
             <p>Its color is <span style={{ color: props.pompompurin.color }}>{props.pompompurin.color}</span></p>
 
             <br />
