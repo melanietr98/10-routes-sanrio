8726ebe0bcbeede2427ed949d593b4f700928403
Melanie R <102996216+melanietr98@users.noreply.github.com>
2023-03-14 00:40:37 -0400

    Update Show.jsx

--- views/helloKitty/Show.jsx
+++ views/helloKitty/Show.jsx
@@ -4,7 +4,7 @@ function Show(props) {
     return (
         <div>
             <h1>Show View</h1>
-            <p>The <strong>{props.helloKitty.name}</strong> is {props.helloKitty.size}</p>
+            <p> <strong>{props.helloKitty.name}</strong> is {props.helloKitty.size}</p>
             <p>Its color is <span style={{ color: props.helloKitty.color }}>{props.helloKitty.color}</span></p>
 
             <br />
